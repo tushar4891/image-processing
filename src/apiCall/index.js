b901a18bb324062a688c95feca307a6d62ef912b
tushar4891 <65764742+tushar4891@users.noreply.github.com>
2024-09-17 09:37:01 +0530

    Update index.js

--- src/apiCall/index.js
+++ src/apiCall/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://image-processing-4.onrender.com",
   //   headers: {
   //     authorization: `Bearer ${localStorage.getItem("token")}`,
   //   },
